767060b04f0cdfa766ba444dabf2b201a5ba7006
JulioMontas <julio86@gmail.com>
2022-07-22 20:22:58 -0400

    url updates

--- js/angular/data/languageList.js
+++ js/angular/data/languageList.js
@@ -1,6 +1,13 @@
 var app = angular.module('store', []);
     var gems = [
       {
+        name: 'ActionScript',
+        year: '1998',
+        paradigm: 'Scripting',
+        url: 'actionscript.html',
+        color:'lang-1980'
+      },
+      {
         name: 'Ada',
         year: '1980',
         paradigm: 'Multi-paradigm',
@@ -8,25 +15,32 @@ var app = angular.module('store', []);
         color:'lang-1980'
       },
       {
-        name: 'ActionScript',
-        year: '1998',
-        paradigm: 'Scripting',
-        url: 'Actionscript.html',
-        color:'lang-1980'
+        name: 'ALGO 58',
+        year: '1958',
+        paradigm: 'Multi-paradigm',
+        url: 'algol_58.html',
+        color:'lang-1950'
       },
       {
-        name: 'Basic',
+        name: 'ALGO 60',
+        year: '1958',
+        paradigm: 'Multi-paradigm',
+        url: 'algol_60.html',
+        color:'lang-1950'
+      },
+      {
+        name: 'ArnoldC',
+        year: '2014',
+        paradigm: 'General-purpose',
+        url: 'arnoldc.html',
+        color:'lang-2000'
+      },
+      {
+        name: 'BASIC',
         year: '1964',
         paradigm: 'General-purpose',
         url: 'basic.html',
         color:'lang-1950'
-      },
-      {
-        name: 'Brainfuck',
-        year: '1993',
-        paradigm: 'Esoteric',
-        url: 'brainfuck.html',
-        color:'lang-1980'
       },
       {
         name: 'Boo',
@@ -34,6 +48,13 @@ var app = angular.module('store', []);
         paradigm: 'Object-oriented',
         url: 'boo.html',
         color:'lang-2000'
+      },
+      {
+        name: 'Brainfuck',
+        year: '1993',
+        paradigm: 'Esoteric',
+        url: 'brainfuck.html',
+        color:'lang-1980'
       },
       {
         name: 'C',
@@ -53,7 +74,7 @@ var app = angular.module('store', []);
         name: 'C++',
         year: '1983',
         paradigm: 'Multi-paradigm',
-        url: 'C++.html',
+        url: 'c++.html',
         color:'lang-1980'
       },
       {
@@ -71,78 +92,15 @@ var app = angular.module('store', []);
         color:'lang-2000'
       },
       {
-        name: 'Cinder',
-        year: '2010',
-        paradigm: 'C++ Library',
-        url: 'cinder.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'D3.js',
-        year: '2011',
-        paradigm: 'JavaScript Library',
-        url: 'd3js.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Django',
-        year: '2005',
-        paradigm: 'Python Web Framework',
-        url: 'django.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'TypeScript',
-        year: '2012',
+        name: 'CLU',
+        year: '1974',
         paradigm: 'Multi-paradigm',
-        url: 'typescript.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Etoys',
-        year: '1996',
-        paradigm: 'Object-oriented',
-        url: 'etoys.html',
-        color:'lang-1980'
-      },
-      {
-        name: 'Euphoria',
-        year: '1993',
-        paradigm: 'Interpreted',
-        url: 'euphoria.html',
-        color:'lang-1980'
-      },
-      {
-        name: 'ECMAScript',
-        year: '1997',
-        paradigm: 'Object-oriented',
-        url: 'etoys.html',
-        color:'lang-1980'
-      },
-      {
-        name: 'Fantom',
-        year: '2005',
-        paradigm: 'Multi-paradigm',
-        url: 'fantom.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'FALSE',
-        year: '1997',
-        paradigm: 'Esoteric',
-        url: 'FALSE.html',
-        color:'lang-1980'
-      },
-      {
-        name: 'Fortran',
-        year: '1957',
-        paradigm: 'Imperative',
-        url: 'fortran.html',
+        url: 'clu.html',
         color:'lang-1950'
       },
       {
         name: 'Forth',
-        year: '1970',
+        year: '1970s',
         paradigm: 'Multi-paradigm',
         url: 'forth.html',
         color:'lang-1950'
@@ -150,8 +108,15 @@ var app = angular.module('store', []);
       {
         name: 'Go',
         year: '2009',
-        paradigm: 'Multi-paradigm',
+        paradigm: 'Multi-Paradigm',
         url: 'go.html',
+        color:'lang-2000'
+      },
+      {
+        name: 'Halide',
+        year: '2012',
+        paradigm: 'Domain-specific',
+        url: 'halide.html',
         color:'lang-2000'
       },
       {
@@ -160,20 +125,6 @@ var app = angular.module('store', []);
         paradigm: 'Functional',
         url: 'haskell.html',
         color:'lang-1980'
-      },
-      {
-        name: 'Haxe',
-        year: '2005',
-        paradigm: 'Multi-paradigm',
-        url: 'haxe.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Halide',
-        year: '2012',
-        paradigm: 'Domain-specific',
-        url: 'haxe.html',
-        color:'lang-2000'
       },
       {
         name: 'Java',
@@ -186,84 +137,28 @@ var app = angular.module('store', []);
         name: 'JavaScript',
         year: '1995',
         paradigm: 'Scripting',
-        url: 'haxe.html',
+        url: 'javascript.html',
         color:'lang-1980'
       },
       {
         name: 'Lisp',
         year: '1958',
         paradigm: 'Multi-paradigm',
-        url: 'java.html',
+        url: 'list.html',
         color:'lang-1950'
       },
       {
         name: 'MATLAB',
         year: '1970',
         paradigm: 'Multi-paradigm',
-        url: 'haxe.html',
+        url: 'matlab.html',
         color:'lang-1950'
-      },
-      {
-        name: 'Modula 3',
-        year: '1980',
-        paradigm: 'Multi-paradigm',
-        url: 'Modula3.html',
-        color:'lang-1980'
       },
       {
         name: 'Objective-C',
         year: '1983',
         paradigm: 'Object-oriented',
         url: 'objective_c.html',
-        color:'lang-1980'
-      },
-      {
-        name: 'Modula 3',
-        year: '2008',
-        paradigm: 'Multi-paradigm',
-        url: 'objective_j.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Opa',
-        year: '2011',
-        paradigm: 'Multi-paradigm',
-        url: 'opa.html',
-        color:'lang-1980'
-      },
-      {
-        name: 'openFrameworks',
-        year: '2005',
-        paradigm: 'C++ Toolkit',
-        url: 'objective_j.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Pascal',
-        year: '1970',
-        paradigm: 'Imperative',
-        url: 'pascal.html',
-        color:'lang-1950'
-      },
-      {
-        name: 'Paper.js',
-        year: '2011',
-        paradigm: 'Vector Graphics Scripting',
-        url: 'paperjs.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Python',
-        year: '1991',
-        paradigm: 'Scripting',
-        url: 'python.html',
-        color:'lang-1980'
-      },
-      {
-        name: 'PHP',
-        year: '1995',
-        paradigm: 'Scripting',
-        url: 'php.html',
         color:'lang-1980'
       },
       {
@@ -274,25 +169,18 @@ var app = angular.module('store', []);
         color:'lang-1980'
       },
       {
-        name: 'Prolog',
-        year: '1972',
-        paradigm: 'Logic',
-        url: 'prolog.html',
-        color:'lang-1950'
+        name: 'PHP',
+        year: '1995',
+        paradigm: 'Scripting',
+        url: 'php.html',
+        color:'lang-1980'
       },
       {
-        name: 'Processing',
-        year: '2001',
-        paradigm: 'Object-oriented',
-        url: 'processing.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Rust',
-        year: '2010',
-        paradigm: 'Multi-paradigm',
-        url: 'rust.html',
-        color:'lang-2000'
+        name: 'Python',
+        year: '1991',
+        paradigm: 'Scripting',
+        url: 'python.html',
+        color:'lang-1980'
       },
       {
         name: 'Ruby',
@@ -302,18 +190,11 @@ var app = angular.module('store', []);
         color:'lang-1980'
       },
       {
-        name: 'Ruby on Rails',
-        year: '2004',
-        paradigm: 'Scripting',
-        url: 'rubyonrails.html',
-        color:'lang-2000'
-      },
-      {
-        name: 'Scala',
-        year: '2003',
-        paradigm: 'Multi-paradigm',
-        url: 'scala.html',
-        color:'lang-2000'
+        name: 'Simula',
+        year: '1967',
+        paradigm: 'Object-oriented',
+        url: 'objective_j.html',
+        color:'lang-1950'
       },
       {
         name: 'Smalltalk',
@@ -323,10 +204,10 @@ var app = angular.module('store', []);
         color:'lang-1950'
       },
       {
-        name: 'Spring',
-        year: '2002',
-        paradigm: 'Java Framework',
-        url: 'scala.html',
+        name: 'Swift',
+        year: '2014',
+        paradigm: 'Multi-paradigm',
+        url: 'swift.html',
         color:'lang-2000'
       },
       {
@@ -349,22 +230,9 @@ var app = angular.module('store', []);
         paradigm: 'Interpreted',
         url: 'yorick.html',
         color:'lang-1980'
-      },
-      {
-        name: 'Zimbu',
-        year: '2009',
-        paradigm: 'Experimental',
-        url: 'zimbu.html',
-        color:'lang-2000'
-      }
-      ,
-      {
-        name: '+',
-        paradigm: 'New Language Submission',
-        url: 'newLangs.html'
       }
     ];
-    
+
     app.controller('StoreController', function () {
       this.product = gems;
     });
